ff1c375550bc608ef19973941f797755d6cd6b88
Kernal99 <mike.box@hotmail.com>
2026-04-24 21:55:19 -0700

    Add iOS splash + icons

---
+++ app/head.tsx
@@ -0,0 +1,28 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="apple-touch-icon" href="/icon-180.png" />
+
+      <link
+        rel="apple-touch-startup-image"
+        href="/apple-splash-1170-2532.png"
+        media="(device-width: 390px) and (device-height: 844px)"
+      />
+      <link
+        rel="apple-touch-startup-image"
+        href="/apple-splash-1290-2796.png"
+        media="(device-width: 430px) and (device-height: 932px)"
+      />
+      <link
+        rel="apple-touch-startup-image"
+        href="/apple-splash-1125-2436.png"
+        media="(device-width: 375px) and (device-height: 812px)"
+      />
+      <link
+        rel="apple-touch-startup-image"
+        href="/apple-splash-828-1792.png"
+        media="(device-width: 414px) and (device-height: 896px)"
+      />
+    </>
+  );
+}
